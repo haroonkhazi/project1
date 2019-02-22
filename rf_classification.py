ad4b27941148004b3d9006c226f86c495c09379e
Haroon Khazi <haroonkhazi@gmail.com>
2019-02-21 19:18:55 -0500

    ran main

--- rf_classification.py
+++ rf_classification.py
@@ -10,18 +10,6 @@ from sklearn.metrics import accuracy_score
 data = pd.read_csv("training.csv")
 
 
-def plotNum(ind):
-    plt.imshow(np.reshape(np.array(data.iloc[ind,1:]), (28, 28)), cmap="gray")
-
-
-#plt.figure()
-#for ii in range(1,17):
-#    plt.subplot(4,4,ii)
-#    plotNum(ii)
-#plt.show()
-
-
-
 X = data.iloc[:, 1:]
 y = data['label']
 
